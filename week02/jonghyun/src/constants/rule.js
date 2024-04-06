418471525d72283f96d8aac40e77a6e8de72c993
Jong1co <narira0922@gmail.com>
2024-04-07 08:14:40 +0900

    삭제

--- week02/jonghyun/src/constants/rule.js
+++
@@ -1,9 +0,0 @@
-const MIN_VALUE = 1;
-const MAX_VALUE = 9;
-const STAGE = 3;
-
-export const VALUE = {
-  MIN: MIN_VALUE,
-  MAX: MAX_VALUE,
-  STAGE,
-};
--- week02/jonghyun/src/view/Input.js
+++
@@ -1,9 +0,0 @@
-import { ENTER_NUMBER } from "../constants/view.js";
-import { Util } from "../utils/index.js";
-
-// export class Input {
-//   static async makeAnAttempt() {
-//     const result = await Util.readLine(ENTER_NUMBER);
-//     return result;
-//   }
-// }
